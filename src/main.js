a26911e9334c6e228b749e2cbbfe8805a2c67def
Greta911 <grema20012000@yahoo.it>
2026-05-23 15:39:25 +0200

    Articulation des components

---
+++ src/main.js
@@ -0,0 +1,10 @@
+//Model de la base de données ou API
+
+import TodoList from "./components/todoList/TodoList";
+
+
+//Instancier un nouveau model Todolist
+new TodoList({
+  el: "#app",
+  apiURL: "https://6a118ab73e35d0f37ee3650e.mockapi.io/"
+});
